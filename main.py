7f273c28ff43631f64320cfe7dc41cfb16743a91
Велигой <k>
2022-05-27 13:21:18 +0300

    found simbol 'c'

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 string = input()
 n = 0
+m = 0
 for i in string:
     n = n+1
     if n == 3:
         continue
+    if i == 'c':
+       m = m + 1
     print(i)
+if m>0:
+    print("found simbol 'c'")
+else:
+    print("simbol 'c' not foud")
 
 
